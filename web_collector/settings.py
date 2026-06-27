dc596336f6f3db9fc33dbc4c779266c87088e03d
fxy <79632345@qq.com>
2026-06-27 18:58:18 +0800

    增加一些防爬措施

--- web_collector/settings.py
+++ web_collector/settings.py
@@ -11,7 +11,7 @@ NEWSPIDER_MODULE = "web_collector.spiders"
 ROBOTSTXT_OBEY = False
 CONCURRENT_REQUESTS = 4
 DOWNLOAD_DELAY = 1.0
-COOKIES_ENABLED = False
+COOKIES_ENABLED = True
 
 DOWNLOADER_MIDDLEWARES = {
     "web_collector.middlewares.ProxyMiddleware": 100,
@@ -23,8 +23,18 @@ DEFAULT_REQUEST_HEADERS = {
         "AppleWebKit/537.36 (KHTML, like Gecko) "
         "Chrome/120.0.0.0 Safari/537.36"
     ),
-    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8",
     "Accept-Language": "zh-CN,zh;q=0.9,en;q=0.8",
+    "Accept-Encoding": "gzip, deflate, br",
+    "Connection": "keep-alive",
+    "Upgrade-Insecure-Requests": "1",
+    "Sec-Fetch-Dest": "document",
+    "Sec-Fetch-Mode": "navigate",
+    "Sec-Fetch-Site": "none",
+    "Sec-Fetch-User": "?1",
+    "Sec-Ch-Ua": '"Not_A Brand";v="8", "Chromium";v="120", "Google Chrome";v="120"',
+    "Sec-Ch-Ua-Mobile": "?0",
+    "Sec-Ch-Ua-Platform": '"Windows"',
 }
 
 ITEM_PIPELINES = {
